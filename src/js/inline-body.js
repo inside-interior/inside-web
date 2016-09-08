d9b29d6172f7e166b6fd30598b6eb5b21b2cd9b2
Sutrisno Suryajaya <sutris.sdp@gmail.com>
2016-09-08 21:22:40 +0800

    Add Google Analytics

--- src/js/inline-body.js
+++ src/js/inline-body.js
@@ -15,3 +15,13 @@ axis.load().then(function() {
 	document.documentElement.classList.add('axis-loaded');
 	localStorage.axisLoaded = true;
 });
+
+// >>>>>> Google analytics script START
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-83893933-1', 'auto');
+ga('send', 'pageview');
+// >>>>>> Google analytics script END
